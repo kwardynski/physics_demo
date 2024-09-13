8fd256c0e85066e3afc1fce7719a381390a68ece
Kacper Wardynski <kacper.wardynski@gmail.com>
2024-09-12 23:56:50 -0600

    chore: cleanup (#6)
    * handle board init
    
    * no more magic numbers

--- assets/js/hooks/ball.js
+++ assets/js/hooks/ball.js
@@ -2,33 +2,30 @@ import * as d3 from "d3";
 
 let BallHook = {
   mounted() {
+    this.handleEvent("init", ({ width, height }) => {
+      svg.attr("width", width).attr("height", height);
+
+      svg
+        .append("rect")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("fill", "none")
+        .attr("stroke", "black");
+
+      svg
+        .append("circle")
+        .attr("cx", 20)
+        .attr("cy", 100)
+        .attr("r", 5)
+        .attr("stroke", "white")
+        .attr("fill", "white");
+    });
+
     this.handleEvent("update-circle", ({ x, y }) => {
       d3.select("circle").style("fill", "black").attr("cx", x).attr("cy", y);
     });
 
-    var width = 300;
-    var height = 300;
-
-    var svg = d3
-      .select("#ball-field")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height);
-
-    svg
-      .append("rect")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("fill", "none")
-      .attr("stroke", "black");
-
-    svg
-      .append("circle")
-      .attr("cx", 20)
-      .attr("cy", 100)
-      .attr("r", 5)
-      .attr("stroke", "white")
-      .attr("fill", "white");
+    var svg = d3.select("#ball-field").append("svg");
   },
 };
 
