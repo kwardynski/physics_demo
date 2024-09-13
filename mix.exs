7bf0bc4ce88b5c6b2300069aceca3f662db5a6e4
Kacper Wardynski <kacper.wardynski@gmail.com>
2024-09-12 19:03:30 -0600

    eps: add `:games_engine` (#3)

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule PhysicsDemo.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:games_engine, git: "https://github.com/kwardynski/games_engine", branch: "main"}
     ]
   end
 
